33a6104966b39b96c79288bcc9d5bed171eddf16
Ayan Ansari <mayan9917@gmail.com>
2026-04-06 23:32:34 +0530

    added the sameSite attribute

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -25,7 +25,7 @@ export const loginUser = async (req, res, next) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: expiresIn,
     });
 
