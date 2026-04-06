e69bd164c5959f88b280988b4bfbfac55329a0c9
Ayan Ansari <mayan9917@gmail.com>
2026-04-06 22:09:24 +0530

    installed dotenv package

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
 import userRoutes from "./src/routes/userRoutes.js";
 import authRoutes from "./src/routes/authRoutes.js";
@@ -7,11 +8,12 @@ import dashboardRoutes from "./src/routes/dashboardRoutes.js";
 import errorHandler from "./src/utils/errorHandler.js";
 import { connectDB } from "./src/config/db.js";
 import cookieParser from "cookie-parser";
+dotenv.config();
 
 await connectDB();
 
 const app = express();
-app.use(cookieParser()); 
+app.use(cookieParser());
 app.use(express.json());
 
 app.use(
@@ -23,9 +25,12 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/records",recordRoutes);
+app.use("/api/records", recordRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
+app.get("/", (req, res) => {
+  res.json({ message: "Finance Dashboard API is running" });
+});
 
 app.use(errorHandler);
 
